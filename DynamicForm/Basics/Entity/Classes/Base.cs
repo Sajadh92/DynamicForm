ceadbb1df20c11832694559596588b97b96da408
Sajjad Al-khafaji <sajadh92@gmail.com>
2024-01-17 14:57:05 +0300

    adding name to base

--- DynamicForm/Basics/Entity/Classes/Base.cs
+++ DynamicForm/Basics/Entity/Classes/Base.cs
@@ -4,5 +4,6 @@ public class Base
 {
     public required Guid Id { get; set; }
     public required int Sort { get; set; }
+    public required string Name { get; set; }
     public Style? Style { get; set; }
 }
